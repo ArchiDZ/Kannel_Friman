658433bd3b4205f699265dbbe875df01a4600150
ArchiDZ <adarznieks.zabelevskiy@yandex.ru>
2022-02-23 18:09:33 +0300

    imit

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,14 @@ public class Main {
 
     public static void main(String[]args){
 
-        Glava4.zadanie_4_11();
-
+        Glava4.zadanie_4_26();
+    /*
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println("================================================");
+        System.out.println(
+        System.getProperty("java.library.path"));
+    */
 
     }
 }
---
+++ src/Razdel5/Glava5.java
@@ -0,0 +1,13 @@
+package Razdel5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Glava5 {
+    static Scanner input = new Scanner(System.in);
+    static Random rnd = new Random();
+
+    public static void zadanie_5_1(){
+
+    }
+}
