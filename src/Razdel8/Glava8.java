0c837b22ee9d1321b2e012d02b9c514ee603ec3b
ArchiDZ <adarznieks.zabelevskiy@yandex.ru>
2022-03-04 16:29:02 +0300

    imit

--- src/Razdel8/Glava8.java
+++ src/Razdel8/Glava8.java
@@ -99,4 +99,75 @@ public class Glava8 {
             System.out.println("==>"+x);
         }else{x-=1; System.out.println("==>"+x);}
     }
+    public void zadanie_8_6(int a,int b){
+        int x;
+        if(a*2!=b)
+            x=a/2+b;
+        else x=b/2+a;
+        if(x<15)
+            System.out.println("X="+x);
+        else System.out.println(x);
+
+       /*
+        Результат:
+        16
+        X=8
+        X=12
+        X=12
+        X=10
+        21
+        18
+               */
+    }
+    public static void zadanie_8_7(){
+        int num1 = rnd.nextInt(150-1);
+        int num2 = rnd.nextInt(150-1);
+        int result;
+            System.out.println("Сгенерировано два случайных числа");
+            System.out.println("I-->"+num1+" II-->"+num2);
+        if(num1>num2){
+            result = num1-num2;
+            System.out.println("Разница между первым и вторым числом-->"+result);
+        }else {
+            result = num2-num1;
+            System.out.println("Разница между вторым и первым -->"+result);
+        }
+        if (num1==num2) {
+            result = num1 * num2;
+            System.out.println("Значения равны друг другу-->" + result);
+        }
+        }
+        public void zadanie_8_8(int a, int b){
+        int c = a/2+b/2;
+        System.out.println("Начальное значение С "+c);
+        if(a+b!=c){
+            c=a/2+b/2;
+            System.out.println("C-->"+c);
+        }
+        else {
+            c=a%2+b%2;
+            System.out.println("C==>"+c);
+        }
+            /*
+             *Начальное значение С 6
+             * C-->6
+             * Начальное значение С 5
+             * C-->5
+             * Начальное значение С 4
+             * C-->4
+              */
+    }
+public void zadanie_8_9(int a, int b, int c){
+        //int a,b,c;
+        if (a%2!=1){
+            b=a/2;
+            c = (a+b)%10;
+        }else{
+            b=a/2+1;
+            c=(a*b)%10;
+            System.out.println(c-a-b);
+        }
+
+}
+
 }
